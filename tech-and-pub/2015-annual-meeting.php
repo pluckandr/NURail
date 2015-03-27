6d29f3120e9ce19442e9681ab1352e0c45b90061
fieldsfarmer <fieldsfarmer@gmail.com>
2015-03-27 13:39:36 -0400

    change on item 9
    I followed the requirement of item 9 from the doc file. I added a php
    called 2015-annual-meeting.php and other stuff like images or pdf file.

---
+++ tech-and-pub/2015-annual-meeting.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<link rel="shortcut icon" href="../logo.png">
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>2015 NURail Annual Meeting</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../resources/css/style.css">
+    </head>
+    <body>
+        <nav>
+            <?php 
+                $page = "about";
+                require_once(__DIR__.'/../includes/header.php');
+            ?>
+        </nav>
+        <section>
+            <div class="section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div style="" class="col-sm-8 panel panel-body">
+                            
+                            
+                            
+<h1> 2015 NURail Annual Meeting</h1> 
+                            
+ <a class="anchor" id="University of Illinois at Urbana-Champaign"></a>                           
+<h4><a>Overview</a></h4>
+ <br>
+
+ 
+<h3 style="color: red">2015 NURail Annual Meeting - Chicago <br>
+ 
+Wednesday, June 3, 2015 - Thursday, June 4, 2015
+</h3>
+	
+	<hr class="clear-both short" /> 
+
+<a class="anchor" id="University of Illinois at Chicago"></a>
+<h4><a>Agenda</a></h4>
+	
+Conference runs from 1:00 p.m. Wednesday until 3:30 p.m. Thursday to allow travelers a single overnight stay. For more information, click <a target="_blank" href="./images/meeting/15agenda.pdf">HERE</a>.
+
+
+<hr class="clear-both short" /> 
+<a class="anchor" id="Massachusetts Institute of Technology"></a>
+<h4><a>Location</a></h4>
+
+University of Illinois at Chicago, Student Center West, 828 S. Wolcott Avenue, Chicago, IL 60612<br>
+<h4><img src="./images/15map.png" style="height: 300px; width: 600px"></h4>
+<br>
+<h4><img src="./images/15building.jpg" style="height: 330px; width: 600px"></h4>
+	
+	<hr class="clear-both short" /> 
+
+
+
+<a class="anchor" id="Michigan Technological University"></a>
+<h4><a>Hotel Info</a></h4>
+Club Quarters - Chicago at Central Loop, 111 West Adams Street, Chicago, IL 60603
+<br>
+<br>
+<table border="l" style="width: 100%">
+  <tr>
+    <td><b>Date</b></td>
+    <td><b>Room Type</b></td> 
+    <td><b>Rate</b></td>
+  </tr>
+  <tr>
+    <td>6/2/15</td>
+    <td>Club<br>Standard</td> 
+    <td>$182<br>$197</td>
+  </tr>
+  <tr>
+    <td>6/3/15</td>
+    <td>Club<br>Standard</td> 
+    <td>$182<br>$197</td>
+  </tr>
+  <tr>
+    <td>6/4/15</td>
+    <td>Club<br>Standard</td> 
+    <td>$157<br>$172</td>
+  </tr>
+</table>
+(Club rooms are one full size bed, 210 sq ft while standard rooms are one queen or two twins upon request, 230 sq ft.)
+<br>
+<br>
+Reserve your room by calling <em>Club Quarters Member Services</em>. Member Services can be reached at:<br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   (203) 905-2100 on Monday - Friday 7a-11p, Saturday 9a-5p and Sunday 10a-6p (EST).
+<br>
+Please provide the Group Code: UOI603 when booking. The cut-off date is May 3, 2015.
+<br>
+<h4><img src="./images/15street.jpg" style="height: 300px; width: 600px"></h4>
+<br>
+ 
+	
+
+<hr class="clear-both short" /> 
+
+<h3 style="color: red">We hope to see you in Chicago!</h3>
+
+
+<!-- <a class="anchor" id="University of Kentucky"></a>
+<h4><a target="_blank" href="http://www.uky.edu/">University of Kentucky </a></h4>
+
+
+ 
+
+	University of Kentucky (UK) was a leader in the development of the rail system in the United States, having one of the first operative railroads in 1831.  Its rail system has had a major presence on the development and sustainability of the state’s coal industry since the very early 1900s.   Several of this nation’s major north-south heavy traffic rail lines -- CSX, NS, and CN – pass through Kentucky.  These comprise the bulk of Kentucky’s over 2,500 miles of track. This is augmented with local collector lines mainly operated by smaller rail companies.  <br><br>
+	The Civil Engineering Department at the University of Kentucky was an early leader among universities in the re-establishment of an academic emphasis in railway transportation beginning in the early 1980s.  The Rail-Related Research Program is a major component of UK’s railway emphasis.  Numerous Research Projects have been conducted during the past thirty years.  These are funded by a combination of railroad companies, governmental agencies, and individuals.  Over fifty graduate and undergraduate students have participated in these studies.
+	
+	<hr class="clear-both short" /> 
+
+
+
+
+<a class="anchor" id="University of Tennessee"></a>
+<h4><a target="_blank" href="http://www.utk.edu/">University of Tennessee, Knoxville</a></h4><h4><img src="images/logos/utk.jpg"/></h4>
+ 
+	The University of Tennessee, Knoxville (UTK) has been a national leader in transportation research and education for over 50 years. Students with particular interests in rail transportation are welcomed in the UT Rail Society, which is also an AREMA student chapter. <br><br>
+	NURail center activities are coordinated by the UTK Center for Transportation Research (CTR). One of the oldest dedicated academic centers focusing on transport, CTR celebrated its 40th anniversary in 2012. CTR has a long tradition of excellence in rail transportation research and education, with particular strengths in economics, planning, safety, and workforce development. CTR has been a provider of education and training to the railroad industry for over 25 years. Current courses address railroad track and structures design, safety inspection, and maintenance. CTR has strong relationships with the railroad and planning offices at the Tennessee Department of Transportation, with Class 1 and shortline railroads operating in the southeast, with railroad contractors and suppliers, and with Oak Ridge National Laboratory. CTR will be the host of the 2013 Joint Rail Conference.
+	
+	<hr class="clear-both short" /> 
+
+<a class="anchor" id="Rose-Hulman Institute of Technology"></a>
+<h4><a target="_blank" href="http://www.rose-hulman.edu/">Rose-Hulman Institute of Technology</a></h4>
+<h4><img style="width:199px" src="images/logos/rhit.png"/></h4><br>
+
+ 
+
+	Rose-Hulman Institute of Technology (Rose-Hulman) is home to 2,100 undergraduate and 100 graduate students majoring in engineering, science or mathematics.  Our mission is to provide our students with the world's best undergraduate science, engineering, and mathematics education in an environment of individual attention and support.   With a 1:13 faculty-student ratio our faculty can take a personal interest in each student's success at Rose-Hulman.  As we prepare students for their careers, our emphasis is on a hands-on education where students learn the value of teamwork as well as formulas and equations.  Rose-Hulman has been ranked No. 1 by U.S. News & World Report among institutions whose highest degree in engineering is the master's.  Among individual engineering programs, Rose-Hulman ranked No. 1 in five areas: chemical engineering, civil engineering, computer engineering, electrical engineering and mechanical engineering.<br><br>
+ -->
+                            
+                            
+                            
+                            </div>
+							
+							
+							
+			<div class="col-sm-4">
+                <h4>Links</h4>
+                <hr />
+                <div class="feature-border">
+                  <div class="feature-box right-col">
+                    <ul>
+					  <li>
+                        <a href="#University%20of%20Illinois%20at%20Urbana-Champaign">Overview</a>
+                      </li>
+					  <li>
+                        <a href="#University of Illinois at Chicago">Agenda</a>
+                      </li>
+					  					  <li>
+                        <a href="#Massachusetts Institute of Technology">Location</a>
+                      </li>
+                      <li>
+                        <a href="#Michigan%20Technological%20University">Hotel Info</a>
+                      </li>
+
+                      <!-- <li>
+                        <a href="#University%20of%20Kentucky">University of Kentucky</a>
+                      </li>
+
+                      <li>
+                        <a href="#University%20of%20Tennessee">University of Tennessee</a>
+                      </li>
+					  
+					                        <li>
+                        <a href="#Rose-Hulman%20Institute%20of%20Technology">Rose-Hulman Institute of Technology</a>
+                      </li> -->
+                    </ul>
+                  </div>
+                </div>
+              </div>
+			  
+			  
+			  
+			  
+                        </div>
+						
+						              
+						
+						
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <?php require_once(__DIR__.'/../includes/footer.php'); ?> 
+
+            
+            <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+            <script src="../resources/js/script.js"></script>
+        </footer>
+    </body>
+</html>
